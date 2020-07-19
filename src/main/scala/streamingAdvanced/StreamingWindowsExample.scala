1752b0161c60db9dbb3f20d768a682ae0683b256
harjeet88 <harjeet.kumar24@gmail.com>
2020-07-19 16:59:01 +0530

    added windows package, docker and netcat install commands

---
+++ src/main/scala/streamingAdvanced/StreamingWindowsExample.scala
@@ -0,0 +1,59 @@
+package streamingAdvanced
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType, TimestampType}
+
+object StreamingWindowsExample {
+  val spark = SparkSession.builder()
+    .appName("Windows Example")
+    .master("local[2]")
+    .getOrCreate()
+
+  def readSocket(): DataFrame ={
+    val df = spark.readStream
+      .format("socket")
+      .option("host","localhost")
+      .option("port",12345)
+      .load()
+      .select(from_json(col("value"),salesSchema).as("sales"))
+      .selectExpr("sales.*")
+
+    return df
+  }
+
+  val salesSchema=StructType(Array(
+    StructField("id",IntegerType),
+    StructField("time",TimestampType),
+    StructField("item",StringType),
+    StructField("quantity",IntegerType)
+  ))
+
+  def tumblingWindowDemo(): Unit ={
+    val df =readSocket()
+    val windowCol = window(col("time"),"5 minutes").as("timeCol")
+
+    val counts = df.groupBy(windowCol)
+      .agg(sum("quantity").as("totalQuantity"))
+      .select(
+        col("timeCol").getField("start").as("start"),
+        col("timeCol").getField("end").as("end"),
+        col("totalQuantity")
+      )
+
+    writeQuery(counts)
+  }
+
+  def writeQuery(df:DataFrame)={
+    df.writeStream
+      .format("console")
+      .outputMode("complete")
+      .start()
+      .awaitTermination()
+  }
+
+  def main(args: Array[String]): Unit = {
+    tumblingWindowDemo()
+  }
+
+}
